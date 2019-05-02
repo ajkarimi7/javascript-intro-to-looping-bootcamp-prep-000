90bd2ec5af451bbea5a87d41461009515a5813d7
ajkarimi7 <ajkarimi7@gmail.com>
2019-05-02 05:12:48 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -17,12 +17,13 @@ function whileLoop(i) {
 }
 
 function doWhileLoop(num) {
-  var a = 0;
-  
-  function incrementVariable() {
-    a = a + 1;
-  return a;
-  } 
+	var i = 0;
+
+	function incrementVariable() {
+		i = i + 1;
+    return i;
+	}
+
   do {
     console.log("I run once regardless.");
   } while (incrementVariable() < num);
